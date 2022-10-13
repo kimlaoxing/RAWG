c20a2a64c551a7d29a16ef0064d9af084ba424da
Kevin Maulana <kimlaoxing@gmail.com>
2022-10-13 16:09:59 +0700

    feat(Extension Scene Delegate)

--- RAWG/Delegate/SceneDelegate.swift
+++ RAWG/Delegate/SceneDelegate.swift
@@ -12,29 +12,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        let mainRouter = DefaultRouter(rootTransition: EmptyTransition())
-        let tabs = [mainRouter.makeHomeTab(), mainRouter.makeFavoriteTab(), mainRouter.makeProfileTab()]
-        let home = TabBar(vc: tabs)
-        home.selectedIndex = 0
-        self.window?.rootViewController = home
-        window?.makeKeyAndVisible()
-        window?.windowScene = windowScene
+        willConnectTo(with: scene)
     }
     
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
+    func sceneDidDisconnect(_ scene: UIScene) { }
     
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
+    func sceneDidBecomeActive(_ scene: UIScene) { }
     
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
+    func sceneWillResignActive(_ scene: UIScene) { }
     
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
+    func sceneWillEnterForeground(_ scene: UIScene) { }
     
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
+    func sceneDidEnterBackground(_ scene: UIScene) { }
 }
---
+++ RAWG/Extension/SceneDelegateExtension.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  RAWG
+//
+//  Created by Kevin Maulana on 13/10/22.
+//
+
+import UIKit
+
+extension SceneDelegate {
+    func willConnectTo(with scene: UIScene) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        let mainRouter = DefaultRouter(rootTransition: EmptyTransition())
+        let tabs = [mainRouter.makeHomeTab(),
+                    mainRouter.makeFavoriteTab(),
+                    mainRouter.makeProfileTab()]
+        let home = TabBar(vc: tabs)
+        home.selectedIndex = 0
+        self.window?.rootViewController = home
+        window?.makeKeyAndVisible()
+        window?.windowScene = windowScene
+    }
+}
